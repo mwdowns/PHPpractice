14271520a8552d95c80e476e10793357072cfc2e
Matthew Downs <mwdowns@gmail.com>
2017-12-13 19:09:48 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Practice</title>
+</head>
+<body>
+    <form action="todoList.php" method="GET" name="todoList">
+        Show ToDo List of: <input type="text" name="user" value=""/>
+        <input type="submit" value="Go">
+    </form>
+</body>
+</html>
